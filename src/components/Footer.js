f147af8eb0c5bbd012da7952cc57171cf348e499
Abdullah <abdullah.alioglu@bahcesehir.edu.tr>
2024-09-19 19:50:08 +0300

    edit footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faGithub,
   faLinkedin,
-  faTwitter,
+  faInstagram,
 } from "@fortawesome/free-brands-svg-icons";
 
 import styles from "./Footer.module.css";
@@ -15,21 +15,39 @@ export default function Footer() {
         <span>© 2024 Abdullah Alioğlu. All rights reserved.</span>
       </div>
       <div className={styles.icons}>
-        <FontAwesomeIcon
-          icon={faGithub}
-          size="2x"
-          style={{ color: "#d4d4d4" }}
-        />
-        <FontAwesomeIcon
-          icon={faLinkedin}
-          size="2x"
-          style={{ color: "#d4d4d4" }}
-        />
-        <FontAwesomeIcon
-          icon={faTwitter}
-          size="2x"
-          style={{ color: "#d4d4d4" }}
-        />
+        <a
+          href="https://github.com/AbdllhAlioglu"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon
+            icon={faGithub}
+            size="2x"
+            style={{ color: "#d4d4d4" }}
+          />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/abdullah-alioglu/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon
+            icon={faLinkedin}
+            size="2x"
+            style={{ color: "#d4d4d4" }}
+          />
+        </a>
+        <a
+          href="https://www.instagram.com/aliogluuuabdullah"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon
+            icon={faInstagram}
+            size="2x"
+            style={{ color: "#d4d4d4" }}
+          />
+        </a>
       </div>
     </div>
   );
