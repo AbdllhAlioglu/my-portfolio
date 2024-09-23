b492e7ba32c4a0717036aa82ed38bde47f84819a
Abdullah <abdullah.alioglu@bahcesehir.edu.tr>
2024-09-23 17:20:59 +0300

    Update email functionality to send messages to a specific address

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -1,16 +1,32 @@
 import React, { useState } from "react";
+import axios from "axios";
 import styles from "./ContactInfo.module.css";
 
 function ContactInfo() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
+  const [status, setStatus] = useState(""); // Başarı ya da hata durumu için
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log("Name:", name);
-    console.log("Email:", email);
-    console.log("Message:", message);
+
+    try {
+      // Backend'e POST isteği gönder
+      await axios.post("http://localhost:5001/send-email", {
+        name,
+        email,
+        message,
+      });
+      setStatus(
+        alert(
+          "Thank you! Your message has been successfully sent. We will get back to you shortly."
+        )
+      );
+    } catch (error) {
+      console.error("Error sending message:", error);
+      setStatus("Failed to send message.");
+    }
   };
 
   return (
@@ -50,6 +66,7 @@ function ContactInfo() {
           Send Message
         </button>
       </form>
+      {status && <p>{status}</p>}
     </div>
   );
 }
